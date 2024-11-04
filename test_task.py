b79194155913417e6695c7ce6de484a7d623e666
Emilien Vesin <emilien.vesin@univ-tlse3.fr>
2024-11-04 20:23:54 +0100

    Test qui marche

--- test_task.py
+++ test_task.py
@@ -6,4 +6,5 @@ import numpy.testing as npt
 class TaskTest(unittest.TestCase):
     def test_task(self):
         t = Task()
+        t.work()  # Ajouté pour que ça marche
         npt.assert_allclose(t.a @ t.x, t.b)

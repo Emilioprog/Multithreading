89898594e65a6aeb20076b3fa670b851e25af8eb
Emilien Vesin <emilien.vesin@univ-tlse3.fr>
2024-11-04 20:20:50 +0100

    Test 3

--- test_task.py
+++ test_task.py
@@ -1,25 +1,9 @@
 import unittest
-import numpy as np
+from task import Task
 import numpy.testing as npt
 
 
-class TestMatrixMultiplication(unittest.TestCase):
-    def test_matrix_product(self):
-        # Définir les matrices/vecteurs A, x, et un B différent
-        A = np.array([[1, 2], [3, 4]])
-        x = np.array([1, 1])
-
-        # Définir B different de A @ x
-        B = np.random.rand(
-            2
-        )  # Génère un B aléatoire qui ne devrait pas correspondre à A @ x
-
-        # Calculer le produit A @ x
-        result = A @ x
-
-        # Vérifier que A @ x est proche de B pour provoquer l'erreur
-        npt.assert_allclose(result, B, rtol=1e-7, atol=0)
-
-
-if __name__ == "__main__":
-    unittest.main()
+class TaskTest(unittest.TestCase):
+    def test_task(self):
+        t = Task()
+        npt.assert_allclose(t.a @ t.x, t.b)
